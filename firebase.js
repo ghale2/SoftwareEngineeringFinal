4967cbb493c28d46eae381dcf2b660e723d41e33
jjingwa <jjingwa@twu.edu>
2025-11-24 14:55:44 -0600

    Reorganized project for Firebase hosting + moved frontend into public/

--- firebase.js
+++ firebase.js
@@ -1,13 +1,3 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyA7Rj7M9eedoCDL81yQzrFI2NW3yPuIMJQ",
-  authDomain: "campushub-58e25.firebaseapp.com",
-  projectId: "campushub-58e25",
-  storageBucket: "campushub-58e25.appspot.com",  // FIXED
-  messagingSenderId: "561321546790",
-  appId: "1:561321546790:web:83d7f41e08ad0f1f7742e7",
-  measurementId: "G-3K8SC5N6VX"
-};
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import {
   getAuth,
